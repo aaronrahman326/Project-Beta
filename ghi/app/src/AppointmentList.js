c3c100de26e0f7e957b5a2e591998ec9767c871d
jacobmikehyde <nickcknapp@gmail.com>
2023-01-24 15:56:02 -0500

    sales model and service model change

---
+++ ghi/app/src/AppointmentList.js
@@ -0,0 +1,12 @@
+import React, { useEffect, useState } from 'react';
+
+function AppointmentList() {
+    const [appointments, setAppointments] = useState([]);
+
+    const fetchAppointments = async () => {
+        const response = await fetch( "http://localhost:8080/api/appointments/" );
+
+        const data = await response.json();
+
+        setAppointments(data);
+    }

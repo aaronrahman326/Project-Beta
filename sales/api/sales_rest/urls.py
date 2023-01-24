749729a3f5fb23e46158f56e963a45fec628681a
jacobmikehyde <nickcknapp@gmail.com>
2023-01-23 17:32:18 -0700

    updated urls

--- sales/api/sales_rest/urls.py
+++ sales/api/sales_rest/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
-from .views import 
+from .views import api_list_sales, api_show_sale
+
+
+urlpatterns = [
+    path('sales/', api_list_sales, name='api_list_sales'),
+    path('sales/<int:pk>/', api_show_sale, name='api_show_sale'),
+]

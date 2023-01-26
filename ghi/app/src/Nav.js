af873d8a075469be71e0f6e071518ae5374506c2
jacobmikehyde <nickcknapp@gmail.com>
2023-01-26 17:46:42 -0500

    nav bar dropdown is now workinnnnngsss

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -9,9 +9,9 @@ function Nav() {
         <NavLink className="navbar-brand" to="/">CarCar</NavLink>
         <div className="row top-buffer">
         <div className="col">
-            <div className="dropdown">
+            <div className="dropdown d-grid d-md-block">
                 <button
-                    className="btn btn-secondary dropdown-toggle" 
+                    className="btn btn-primary dropdown-toggle gap-2" 
                     type="button" 
                     id="dropdownMenuButton" 
                     data-bs-toggle="dropdown" 
@@ -20,12 +20,13 @@ function Nav() {
                     Services
                 </button>
                 <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <NavLink className="dropdown-item" to="/technicians">Technicians</NavLink>
-                    <NavLink className="dropdown-item" to="/appointments">Appointments</NavLink>
+                    <NavLink className="dropdown-item" to="/technicians/new">New Technicians</NavLink>
+                    <NavLink className="dropdown-item" to="/appointments">Current Appointments</NavLink>
                     <NavLink className="dropdown-item" to="/appointments/new">Create Appointment</NavLink>
+                    <NavLink className="dropdown-item" to="/appointments/search">Appointment History</NavLink>
                 </div>
                 <button
-                    className="btn btn-secondary dropdown-toggle" 
+                    className="btn btn-secondary dropdown-toggle gap-2" 
                     type="button" 
                     id="dropdownMenuButton" 
                     data-bs-toggle="dropdown" 
@@ -34,13 +35,15 @@ function Nav() {
                     Sales
                 </button>
                 <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <NavLink className="dropdown-item" to="/technicians">Sales List</NavLink>
-                    <NavLink className="dropdown-item" to="/appointments">New Sale</NavLink>
-                    <NavLink className="dropdown-item" to="/appointments">Customer</NavLink>
-                    <NavLink className="dropdown-item" to="/appointments/new">Create Customer</NavLink>
+                    <NavLink className="dropdown-item" to="/sales">Sales List</NavLink>
+                    <NavLink className="dropdown-item" to="/sales/new">New Sale</NavLink>
+                    <NavLink className="dropdown-item" to="/customers">Customer</NavLink>
+                    <NavLink className="dropdown-item" to="/cumstomers/new">Create Customer</NavLink>
+                    <NavLink className="dropdown-item" to="/employees/new">Create Employee</NavLink>
+                    <NavLink className="dropdown-item" to="/employees">Employees</NavLink>
                 </div>
                 <button
-                    className="btn btn-secondary dropdown-toggle" 
+                    className="btn btn-primary dropdown-toggle" 
                     type="button" 
                     id="dropdownMenuButton" 
                     data-bs-toggle="dropdown" 
@@ -49,8 +52,8 @@ function Nav() {
                     Inventory
                 </button>
                 <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-                    <NavLink className="dropdown-item" to="/technicians">Automobiles</NavLink>
-                    <NavLink className="dropdown-item" to="/appointments">Appointments</NavLink>
+                    <NavLink className="dropdown-item" to="/technicians">Automobiles List</NavLink>
+                    <NavLink className="dropdown-item" to="/appointments">Create Automobile</NavLink>
                     <NavLink className="dropdown-item" to="/appointments/new">Create Appointment</NavLink>
                 </div>
             </div>

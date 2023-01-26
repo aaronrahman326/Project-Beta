7b7a05ac6151e964415cd1ea681e13bd37b1411e
jacobmikehyde <nickcknapp@gmail.com>
2023-01-26 17:17:00 -0500

    added nav items

--- ghi/app/src/Nav.js
+++ ghi/app/src/Nav.js
@@ -24,6 +24,35 @@ function Nav() {
                     <NavLink className="dropdown-item" to="/appointments">Appointments</NavLink>
                     <NavLink className="dropdown-item" to="/appointments/new">Create Appointment</NavLink>
                 </div>
+                <button
+                    className="btn btn-secondary dropdown-toggle" 
+                    type="button" 
+                    id="dropdownMenuButton" 
+                    data-bs-toggle="dropdown" 
+                    aria-haspopup="true"
+                    aria-expanded="false">
+                    Sales
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <NavLink className="dropdown-item" to="/technicians">Sales List</NavLink>
+                    <NavLink className="dropdown-item" to="/appointments">New Sale</NavLink>
+                    <NavLink className="dropdown-item" to="/appointments">Customer</NavLink>
+                    <NavLink className="dropdown-item" to="/appointments/new">Create Customer</NavLink>
+                </div>
+                <button
+                    className="btn btn-secondary dropdown-toggle" 
+                    type="button" 
+                    id="dropdownMenuButton" 
+                    data-bs-toggle="dropdown" 
+                    aria-haspopup="true"
+                    aria-expanded="false">
+                    Inventory
+                </button>
+                <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                    <NavLink className="dropdown-item" to="/technicians">Automobiles</NavLink>
+                    <NavLink className="dropdown-item" to="/appointments">Appointments</NavLink>
+                    <NavLink className="dropdown-item" to="/appointments/new">Create Appointment</NavLink>
+                </div>
             </div>
         </div>
     </div>
